7eadc2e0a7ff439dd5eb656c0f9723a35282539a
jhuang429 <jhuang9240@gmail.com>
2020-01-29 10:19:55 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,19 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      "<h1> Welcome to the Nested Forms Lab!</h1>"
+    end
+
+
+    get '/new' do
+      erb :"/pirates/new"
+    end
+
+    post '/pirates' do
+      @data = params
+      erb :"pirates/show"
+    end
 
   end
 end
